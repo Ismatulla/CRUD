10d182f5c37f3e6fd576060eb6ded2cb8937ebe3
ISMATULLA <ismatilloqoyliyev@gmail.com>
2021-09-07 11:42:38 +0200

    almost done

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ const button = document.querySelector('.btn-green')
 const itemContainer = document.querySelector('.item-container')
 const alert = document.querySelector('.alert')
 const itemList = document.querySelector('.item-list')
-
+const clearButton = document.querySelector('.clearButton')
 /* Icons */
 let checkIcon = '<a href="#" class="check icons"><i class="far fa-check-circle"></i></a>' 
 let editIcon = '<a href="#" class="edit icons"><i class="far fa-edit"></i></a>'
 let deleteIcon = '<a href="#" class="delete icons"><i class="far fa-times-circle"></i></a>'
-
-const icons = `${checkIcon} ${editIcon} ${deleteIcon}`
 
 
 /* EVENT LISTENER */
@@ -35,26 +33,55 @@ function warning(){
 /* Function Check */
 
 function check(){
-    let itemIcons = document.createElement('div')
+
+    let checkIcons = document.createElement('button')
+    let editIcons = document.createElement('button')
+    let deleteIcons = document.createElement('button')
     const item = document.createElement('div')
-    const deleteItem = document.createElement('div')
-    deleteItem.append(deleteIcon)
+
+    checkIcons.classList.add('item-icons')
+    editIcons.classList.add('item-icons')
+    deleteIcons.classList.add('item-icons')
+    checkIcons.innerHTML = checkIcon
+    editIcons.innerHTML = editIcon
+    deleteIcons.innerHTML = deleteIcon
+    
+
     item.classList.add('item')
     const h5 = document.createElement('h5')
-    h5.classList.add('item-name')
     h5.innerText = input.value
-    itemIcons.classList.add('item-icons')
-    
-    itemIcons.innerHTML = deleteItem
+   
+    item.append(h5,checkIcons,editIcons,deleteIcons) 
 
-    item.append(h5,itemIcons) 
     itemList.appendChild(item)
     alert.style.display = 'none'
     input.value = ''  
+     
 
- 
+    checkIcons.addEventListener('click',()=>{
+        h5.classList.add('item-name')
+       
+    })
+
+
+    editIcons.addEventListener('click',()=>{
+        item.remove()
+        input.value = h5.innerText
+    })
+    
+    deleteIcons.addEventListener('click',()=>{
+        item.remove()
+    })
+       
+    document.querySelector('.clearButton').addEventListener('click',()=>{
+        item.remove()
         
+    })
+
+   
     }
+   
+     
 
 
-/* CHECKLIST */
+
